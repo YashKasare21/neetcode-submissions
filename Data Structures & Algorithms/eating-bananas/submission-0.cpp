b484030ee9ce1ea6f99ac5b3b418a6f1949b4e9f
Yash Nilesh Kasare <158801892+YashKasare21@users.noreply.github.com>
2026-09-21 15:05:36 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        int ans = right;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            long long hours_needed = 0;
+            for(int pile : piles){
+                hours_needed += (pile + mid - 1) / mid;
+            }
+            if(hours_needed <= h){
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
